e06e0b98363c6546de0d74b91c1f9af649534406
Majid Ghaemifar <mgh145@gmail.com>
2020-06-22 22:56:08 +0430

    add job make command

---
+++ src/Command/MakeJob.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Sedehi\Artist\Console\Command;
+
+use Illuminate\Foundation\Console\JobMakeCommand;
+use Illuminate\Support\Str;
+use Sedehi\Artist\Console\Questions\SectionName;
+use Sedehi\Artist\Console\Traits\CommandOptions;
+use Sedehi\Artist\Console\Traits\Interactive;
+
+class MakeJob extends JobMakeCommand implements SectionName
+{
+    use CommandOptions,Interactive;
+
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        $namespace = $rootNamespace.'\Http';
+        if ($this->option('section') !== null) {
+            $namespace .= '\Controllers\\'.Str::studly($this->option('section'));
+        }
+        return $namespace.'\Jobs';
+    }
+}
