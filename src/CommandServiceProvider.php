a81ee8d85462e222c239d0bd356dcfb4b4c62c62
Majid Ghaemifar <mgh145@gmail.com>
2020-06-24 00:07:31 +0430

    update cast make command to work on laravel 7 and higher

--- src/CommandServiceProvider.php
+++ src/CommandServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Sedehi\Artist\Console;
 
+use Illuminate\Foundation\Console\CastMakeCommand;
 use Illuminate\Foundation\Providers\ArtisanServiceProvider;
 use Sedehi\Artist\Console\Command\MakeCast;
 use Sedehi\Artist\Console\Command\MakeChannel;
@@ -108,8 +109,15 @@ class CommandServiceProvider extends ArtisanServiceProvider
 
     protected function registerCastMakeCommand()
     {
-        $this->app->singleton('command.cast.make', function ($app) {
-            return new MakeCast($app['files']);
-        });
+        $version = explode('.',$this->app->version());
+
+        if (reset($version) >= 7) {
+            $this->app->singleton('command.cast.make', function ($app) {
+                return new MakeCast($app['files']);
+            });
+            return;
+        }
+
+        parent::registerCastMakeCommand();
     }
 }
