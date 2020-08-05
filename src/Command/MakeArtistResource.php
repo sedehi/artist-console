c6d645c33cf5763000331cff017818784f055dfb
Majid Ghaemifar <mgh145@gmail.com>
2020-08-05 15:18:47 +0430

    add interactive to artist resource command

--- src/Command/MakeArtistResource.php
+++ src/Command/MakeArtistResource.php
@@ -5,17 +5,22 @@ namespace Sedehi\Artist\Console\Command;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
+use Sedehi\Artist\Console\Questions\SectionName;
+use Sedehi\Artist\Console\Traits\Interactive;
 
-class MakeArtistResource extends Command
+class MakeArtistResource extends Command implements SectionName
 {
+    use Interactive;
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
     protected $signature = 'make:artist-resource
-        {section : The name of the section}
-        {name : The name of the resource}';
+        {name : The name of the resource}
+        {--section= : The name of the section}
+        {--in : Interactive mode}';
 
     /**
      * The console command description.
@@ -31,7 +36,13 @@ class MakeArtistResource extends Command
      */
     public function handle()
     {
-        $resourcePath = app_path('Http/Controllers/'.Str::studly($this->argument('section')).'/'.Str::studly($this->argument('name')).'.php');
+        $this->interactive();
+
+        $resourcePath = app_path(Str::studly($this->argument('name')).'.php');
+
+        if ($this->option('section')) {
+            $resourcePath = app_path('Http/Controllers/'.Str::studly($this->option('section')).'/'.Str::studly($this->argument('name')).'.php');
+        }
 
         if (File::exists($resourcePath)) {
             $this->error('Resource already exists.');
@@ -39,31 +50,26 @@ class MakeArtistResource extends Command
             return false;
         }
 
-        if (! File::isDirectory(app_path('Http/Controllers/'.Str::studly($this->argument('section'))))) {
-            File::makeDirectory(app_path('Http/Controllers/'.Str::studly($this->argument('section'))), 0775, true);
+        if ($this->option('section')) {
+            if (! File::isDirectory(app_path('Http/Controllers/'.Str::studly($this->option('section'))))) {
+                File::makeDirectory(app_path('Http/Controllers/'.Str::studly($this->option('section'))), 0775, true);
+            }
         }
 
         $data = File::get(__DIR__.'/stubs/artist-resource.stub');
-        $data = str_replace(
-            '{{{DummyNamespace}}}',
-            $this->laravel->getNamespace().'Http\\Controllers\\'.Str::studly($this->argument('section')),
-            $data
-        );
-        $data = str_replace(
-            '{{{DummyFullModelClass}}}',
-            $this->laravel->getNamespace().'Http\\Controllers\\'.Str::studly($this->argument('section')).'\\Models\\'.str_replace('Resource', '', Str::studly($this->argument('name'))),
-            $data
-        );
-        $data = str_replace(
-            '{{{ClassName}}}',
-            Str::studly($this->argument('name')),
-            $data
-        );
-        $data = str_replace(
-            '{{{DummyModel}}}',
-            str_replace('Resource', '', Str::studly($this->argument('name'))),
-            $data
-        );
+
+        $namespace = str_replace('\\','',$this->laravel->getNamespace());
+        $fullModelClass = $this->laravel->getNamespace().str_replace('Resource', '', Str::studly($this->argument('name')));
+
+        if ($this->option('section')) {
+            $namespace = $this->laravel->getNamespace().'Http\\Controllers\\'.Str::studly($this->option('section'));
+            $fullModelClass = $this->laravel->getNamespace().'Http\\Controllers\\'.Str::studly($this->option('section')).'\\Models\\'.str_replace('Resource', '', Str::studly($this->argument('name')));
+        }
+
+        $data = str_replace('{{{DummyNamespace}}}',  $namespace, $data);
+        $data = str_replace('{{{DummyFullModelClass}}}', $fullModelClass, $data);
+        $data = str_replace('{{{DummyModel}}}', str_replace('Resource', '', Str::studly($this->argument('name'))), $data);
+        $data = str_replace('{{{ClassName}}}', Str::studly($this->argument('name')), $data);
 
         File::put($resourcePath, $data);
 
--- tests/Command/MakeArtistResourceTest.php
+++ tests/Command/MakeArtistResourceTest.php
@@ -12,12 +12,37 @@ class MakeArtistResourceTest extends SectionTestCase
      */
     public function make_artist_resource_test()
     {
+        $path = app_path("{$this->sampleName}.php");
+
+        $this->artisan('make:artist-resource', [
+            'name'  => $this->sampleName,
+        ]);
+
+        $this->assertFileExists($path);
+
         $path = app_path('Http/Controllers/'.$this->sectionName."/{$this->sampleName}.php");
 
         $this->artisan('make:artist-resource', [
-            'section'   => $this->sectionName,
             'name'      => $this->sampleName,
+            '--section' => $this->sectionName,
         ]);
+
+        $this->assertFileExists($path);
+    }
+
+    /**
+     * @return void
+     * @test
+     */
+    public function interactive_option()
+    {
+        $path = app_path('Http/Controllers/'.$this->sectionName."/{$this->sampleName}.php");
+
+        $this->artisan('make:artist-resource', [
+            'name'  => $this->sampleName,
+            '--in'  => true,
+        ])->expectsQuestion('Enter section name: [optional]', $this->sectionName)
+            ->assertExitCode(0);
 
         $this->assertFileExists($path);
     }
